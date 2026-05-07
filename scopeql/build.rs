ca4720e14eb30a179a069f4ba267eb5c94ed4c22
tison <wander4096@gmail.com>
2026-05-08 07:41:04 +0800

    build: use gix to populate build envs (#30)
    Signed-off-by: tison <wander4096@gmail.com>

--- scopeql/build.rs
+++ scopeql/build.rs
@@ -12,60 +12,103 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use std::collections::BTreeSet;
 use std::env;
+use std::fmt::Display;
 use std::path::Path;
-use std::str::FromStr;
+use std::process::Command;
 
-use shadow_rs::CARGO_METADATA;
-use shadow_rs::CARGO_TREE;
-use shadow_rs::ShadowBuilder;
-
-fn configure_rerun_if_head_commit_changed() {
-    let mut current = Path::new(env!("CARGO_MANIFEST_DIR")).to_path_buf();
-
-    // skip if no valid-looking git repository could be found
-    while let Ok((dir, _)) = gix_discover::upwards(current.as_path()) {
-        match dir {
-            gix_discover::repository::Path::Repository(git_dir) => {
-                unreachable!(
-                    "build.rs should never be placed in a git bare repository: {}",
-                    git_dir.display()
-                );
-            }
-            gix_discover::repository::Path::WorkTree(work_dir) => {
-                let git_refs_heads = work_dir.join(".git/refs/heads");
-                println!("cargo::rerun-if-changed={}", git_refs_heads.display());
-                break;
-            }
-            gix_discover::repository::Path::LinkedWorkTree { work_dir, .. } => {
-                current = work_dir
-                    .parent()
-                    .expect("submodule's work_dir must have parent")
-                    .to_path_buf();
-                continue;
-            }
-        };
-    }
+fn configure_rustc_env(key: &'static str, value: impl Display) {
+    println!("cargo:rustc-env={key}={value}");
 }
 
-fn main() -> shadow_rs::SdResult<()> {
+fn configure_rustc_env_within_repo(repo: gix::Repository) -> Result<(), gix::Error> {
+    let head = repo.head().map_err(gix::Error::from_error)?;
+    configure_rustc_env(
+        "SCOPEQL_GIT_BRANCH",
+        match &head.kind {
+            gix::head::Kind::Symbolic(r) => r.name.shorten(),
+            gix::head::Kind::Unborn(n) => n.shorten(),
+            gix::head::Kind::Detached { .. } => gix::bstr::BStr::new("(detached)"),
+        },
+    );
+
+    let head_commit = repo.head_commit().map_err(gix::Error::from_error)?;
+    configure_rustc_env("SCOPEQL_GIT_COMMIT_HASH", head_commit.id);
+
+    fn is_dirty(repo: &gix::Repository) -> Result<bool, gix::Error> {
+        let status_platform = repo
+            .status(gix::progress::Discard)
+            .map_err(gix::Error::from_error)?;
+        let status_iter = status_platform
+            .untracked_files(gix::status::UntrackedFiles::Collapsed)
+            .into_iter(None)
+            .map_err(gix::Error::from_error)?;
+        for item in status_iter {
+            let item = item.map_err(gix::Error::from_error)?;
+            let dirty = match item {
+                gix::status::Item::IndexWorktree(item) => match item {
+                    gix::status::index_worktree::Item::Modification { .. } => true,
+                    gix::status::index_worktree::Item::DirectoryContents { entry, .. } => {
+                        matches!(entry.status, gix::dir::entry::Status::Untracked)
+                    }
+                    gix::status::index_worktree::Item::Rewrite { .. } => true,
+                },
+                gix::status::Item::TreeIndex { .. } => true,
+            };
+            if dirty {
+                return Ok(true);
+            }
+        }
+
+        Ok(false)
+    }
+    configure_rustc_env("SCOPEQL_GIT_DIRTY", is_dirty(&repo)?);
+
+    let author = head_commit.author().map_err(gix::Error::from_error)?;
+    let datetime = author.time().map_err(gix::Error::from_error)?;
+    let epoch = datetime.seconds;
+    let source_timestamp = jiff::Timestamp::from_second(epoch).map_err(gix::Error::from_error)?;
+    configure_rustc_env("SCOPEQL_SOURCE_TIMESTAMP", source_timestamp);
+
+    Ok(())
+}
+
+fn main() {
+    // Ensure SCOPEQL_SOURCE_TIMESTAMP is present
+    configure_rustc_env("SCOPEQL_SOURCE_TIMESTAMP", "");
+
     let now = jiff::Timestamp::now();
+    // Truncate to seconds to align with SCOPEQL_SOURCE_TIMESTAMP
+    configure_rustc_env(
+        "SCOPEQL_BUILD_TIMESTAMP",
+        now.strftime("%Y-%m-%dT%H:%M:%SZ"),
+    );
 
-    configure_rerun_if_head_commit_changed();
+    if let Ok(version) = Command::new("rustc").arg("-V").output() {
+        let version = String::from_utf8_lossy(&version.stdout);
+        configure_rustc_env("SCOPEQL_RUSTC_VERSION", version.trim());
+    }
 
-    // The "CARGO_WORKSPACE_DIR" is set manually (not by Rust itself) in Cargo config file, to
-    // solve the problem where the "CARGO_MANIFEST_DIR" is not what we want when this repo is
-    // made as a submodule in another repo.
-    let src_path = env::var("CARGO_WORKSPACE_DIR").or_else(|_| env::var("CARGO_MANIFEST_DIR"))?;
-    let out_path = env::var("OUT_DIR")?;
-    let shadow = ShadowBuilder::builder()
-        .src_path(src_path)
-        .out_path(out_path)
-        // exclude these two large constants that we don't need
-        .deny_const(BTreeSet::from([CARGO_METADATA, CARGO_TREE]))
-        .build()?;
+    if let Ok(target) = env::var("TARGET") {
+        configure_rustc_env("SCOPEQL_BUILD_TARGET", target);
+    }
 
+    // Ensure all Git-related build environment variables are present with default values
+    configure_rustc_env("SCOPEQL_GIT_BRANCH", "");
+    configure_rustc_env("SCOPEQL_GIT_COMMIT_HASH", "");
+    configure_rustc_env("SCOPEQL_GIT_DIRTY", "false");
+
+    // Override Git-related build environment variables if within a Git repository
+    if let Ok(repo) = gix::discover(Path::new(env!("CARGO_MANIFEST_DIR"))) {
+        let git_refs_heads = repo.path().join("refs/heads");
+        println!("cargo::rerun-if-changed={}", git_refs_heads.display());
+
+        if let Err(err) = configure_rustc_env_within_repo(repo) {
+            println!("cargo:warning=failed to configure environment within git repo: {err}");
+        }
+    }
+
+    // Override SCOPEQL_SOURCE_TIMESTAMP with SOURCE_DATE_EPOCH if set
     // @see https://reproducible-builds.org/docs/source-date-epoch/
     println!("cargo:rerun-if-env-changed=SOURCE_DATE_EPOCH");
     if let Some(ts) = env::var_os("SOURCE_DATE_EPOCH") {
@@ -77,20 +120,6 @@ fn main() -> shadow_rs::SdResult<()> {
         let source_timestamp = jiff::Timestamp::from_second(epoch).unwrap_or_else(|err| {
             panic!("SOURCE_DATE_EPOCH could not be cast to a timestamp: {err}")
         });
-        println!("cargo:rustc-env=SOURCE_TIMESTAMP={source_timestamp}");
-    } else if let Some(commit) = shadow.map.get(shadow_rs::COMMIT_DATE_3339) {
-        let ts = commit.v.as_str();
-        let source_timestamp = jiff::Timestamp::from_str(ts).unwrap_or_else(|err| {
-            panic!("COMMIT_DATE_3339 {ts} could not be cast to a timestamp: {err}")
-        });
-        println!("cargo:rustc-env=SOURCE_TIMESTAMP={source_timestamp}");
-    } else {
-        println!("cargo:warning=SOURCE_TIMESTAMP is set to empty");
-        println!("cargo:rustc-env=SOURCE_TIMESTAMP=");
-    };
-
-    let build_timestamp = now;
-    println!("cargo:rustc-env=BUILD_TIMESTAMP={build_timestamp}");
-
-    Ok(())
+        configure_rustc_env("SCOPEQL_SOURCE_TIMESTAMP", source_timestamp);
+    }
 }
