dea87d8cc3b5eaab5f60c36e9f2cd8d6a745b8d8
tison <wander4096@gmail.com>
2026-08-22 20:14:34 +0800

    chore: migrate HawkEye to v7

--- scopeql/scripts/docker-build.sh
+++ scopeql/scripts/docker-build.sh
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR/.."
 
